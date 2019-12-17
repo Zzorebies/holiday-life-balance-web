57b686b131da9baa3461f7ae019978dc7e12b43e
Heath Ryu <32060873+heathryu@users.noreply.github.com>
2019-12-17 21:57:38 +0000

    Add unstyled UI components (#1)

---
+++ src/components/VacationDaysInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const VacationDaysInput = ({ vacationDays, setVacationDays }) => {
+  const [error, setError] = useState(null);
+
+  const handleChange = e => {
+    const value = e.target.value;
+
+    if (value >= 30) {
+      setError('너무 하시는 거 아닌가요? 😨');
+    } else if (value <= 0) {
+      setError('1 이상의 숫자를 입력해 주세요. 😊');
+    } else {
+      setError(null);
+    }
+
+    setVacationDays(e.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="vacation-days">휴가 기간</label>
+      <input
+        type="number"
+        name="vacation-days"
+        onChange={handleChange}
+        value={vacationDays}
+      />{' '}
+      일{error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default VacationDaysInput;
---
+++ src/components/YearPicker.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const YearPicker = ({
+  startingYear,
+  maxNumberOfYears = 5,
+  setSelectedYear
+}) => {
+  const years = [...Array(maxNumberOfYears).keys()].map(n => n + startingYear);
+
+  const handleChange = e => {
+    setSelectedYear(e.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="year">년도</label>
+      <select name="year" onChange={handleChange}>
+        {years.map(year => (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        ))}
+      </select>{' '}
+      년
+    </div>
+  );
+};
+
+export default YearPicker;
