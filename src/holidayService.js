96004a5c7af4f7a9a31b66415547b923b8d2d363
jihyun-um <ashleyum20@gmail.com>
2020-02-09 20:51:20 +0000

    Add functions to get all public holidays and non-weekend holidays by year

--- src/holidayService.js
+++ src/holidayService.js
@@ -1,14 +1,14 @@
+import holidays from './mock/holidays.json';
+
 export const getDates = (rangeStartDateIso, rangeEndDateIso) => {
   const endDate = new Date(rangeEndDateIso);
   let currentDate = new Date(rangeStartDateIso);
 
   const dates = [];
-
   while (currentDate <= endDate) {
     dates.push(currentDate);
     currentDate = addDays(currentDate, 1);
   }
-
   return dates;
 };
 
@@ -19,3 +19,23 @@ export const addDays = (date, days) => {
 };
 
 export const isWeekend = date => date.getDay() === 6 || date.getDay() === 0;
+
+export const getHolidays = year => {
+  const selectedYearHolidays = [];
+  for (let holiday of holidays) {
+    if (holiday.year === year) {
+      selectedYearHolidays.push(holiday);
+    }
+  }
+  return selectedYearHolidays;
+};
+
+export const getNonWeekendHolidays = year => {
+  const nonWeekendHolidays = [];
+  for (let holiday of holidays) {
+    if (holiday.year === year && !isWeekend(new Date(holiday.date))) {
+      nonWeekendHolidays.push(holiday);
+    }
+  }
+  return nonWeekendHolidays;
+};
