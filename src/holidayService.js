5a6e319445ae580474aed8e975dd968e476b6f93
Heath Ryu <32060873+heathryu@users.noreply.github.com>
2020-03-01 16:06:57 +0000

    Add pre-commit hook for linting

--- src/holidayService.js
+++ src/holidayService.js
@@ -5,16 +5,19 @@ export const getDates = (rangeStartDateIso, rangeEndDateIso) => {
   let currentDate = new Date(rangeStartDateIso);
 
   const dates = [];
+
   while (currentDate <= endDate) {
     dates.push(currentDate);
     currentDate = addDays(currentDate, 1);
   }
+
   return dates;
 };
 
 export const addDays = (date, days) => {
   const newDate = new Date(date);
   newDate.setDate(newDate.getDate() + days);
+
   return newDate;
 };
 
@@ -22,20 +25,24 @@ export const isWeekend = date => date.getDay() === 6 || date.getDay() === 0;
 
 export const getHolidays = year => {
   const selectedYearHolidays = [];
+
   for (let holiday of holidays) {
     if (holiday.year === year) {
       selectedYearHolidays.push(holiday);
     }
   }
+
   return selectedYearHolidays;
 };
 
 export const getNonWeekendHolidays = year => {
   const nonWeekendHolidays = [];
+
   for (let holiday of holidays) {
     if (holiday.year === year && !isWeekend(new Date(holiday.date))) {
       nonWeekendHolidays.push(holiday);
     }
   }
+
   return nonWeekendHolidays;
 };
