21c2ebacc4ad22518cb5595690fe515a736fec0a
jihyun-um <ashleyum20@gmail.com>
2020-04-16 14:33:46 +0900

    Use constants for day values

--- src/services/weekendService.js
+++ src/services/weekendService.js
@@ -1,6 +1,8 @@
 import { addDays } from '../services/holidayService';
+import { DAYS, NUM_DAYS_IN_WEEK } from '../utils/constants';
 
-export const isWeekend = date => date.getDay() === 6 || date.getDay() === 0;
+export const isWeekend = date =>
+  date.getDay() === DAYS.saturday || date.getDay() === DAYS.sunday;
 
 export const getPreviousWeekend = date => {
   const currentDate = new Date(date);
@@ -9,14 +11,15 @@ export const getPreviousWeekend = date => {
 
 export const getNextWeekend = date => {
   const currentDate = new Date(date);
-  return addDays(currentDate, 6 - currentDate.getDay());
+  return addDays(currentDate, DAYS.saturday - currentDate.getDay());
 };
 
 export const getNumberOfWeekends = (startDate, vacationDays) => {
   const endDateDay = startDate.getDay() + vacationDays;
 
-  const numberOfFullWeekends = Math.trunc(endDateDay / 7);
-  const numberOfHalfWeekends = endDateDay % 7 === 6 ? 1 : 0;
+  const numberOfFullWeekends = Math.trunc(endDateDay / NUM_DAYS_IN_WEEK);
+  const numberOfHalfWeekends =
+    endDateDay % NUM_DAYS_IN_WEEK === DAYS.saturday ? 1 : 0;
 
   return numberOfFullWeekends * 2 + numberOfHalfWeekends;
 };
---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const DAYS = {
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+  sunday: 0
+};
+
+export const NUM_DAYS_IN_WEEK = 7;
