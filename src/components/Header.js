d181a8b2cf7a42eb0e419a5e44b57322a420ad19
Jihyun Um <54134413+jihyun-um@users.noreply.github.com>
2020-08-16 23:06:57 +0900

    Add Material UI (#13)
    * Add @material-ui/core to dependencies
    
    * Replace current styling with Material UI components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
 import styled from 'styled-components';
 
 const Header = ({ className }) => {
   return (
     <div className={className}>
-      <div className="header-center">
-        <h1>Holiday Life Balance</h1>
-        <h3>최소한의 연차로 최대한의 휴가를 즐기고 싶은 당신을 위해</h3>
-      </div>
+      <Container maxWidth="sm">
+        <div className="header-center">
+          <h1>Holiday Life Balance</h1>
+          <h3>최소한의 연차로 최대한의 휴가를 즐기고 싶은 당신을 위해</h3>
+        </div>
+      </Container>
     </div>
   );
 };
@@ -15,16 +18,12 @@ const Header = ({ className }) => {
 const StyledHeader = styled(Header)`
   background: ${(props) => props.theme.color.serenity};
   color: ${(props) => props.theme.color.white};
-  margin: 0 0 3rem;
+  margin: 0 0 2rem;
   padding: 2rem 0;
 
   .header-center {
-    width: 80%;
+    text-align: center;
     margin: 0 auto;
-
-    ${(props) => props.theme.media.desktop} {
-      width: 60%;
-    }
   }
 
   h1 {
--- src/components/HolidayForm.js
+++ src/components/HolidayForm.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
 import styled from 'styled-components';
-
-import YearPicker from './YearPicker';
-import VacationDaysInput from './VacationDaysInput';
-import Button from './Button';
 import { getHolidayRecommendations } from '../services/holidayRecommendationService';
+import VacationDaysInput from './VacationDaysInput';
+import YearPicker from './YearPicker';
 
 const HolidayForm = ({ className, setHolidayRecommendations }) => {
   const currentYear = new Date().getFullYear();
@@ -24,19 +23,17 @@ const HolidayForm = ({ className, setHolidayRecommendations }) => {
   return (
     <form onSubmit={handleSubmit} className={className}>
       <div className="center-form">
-        <div className="inputs-container">
-          <YearPicker
-            setSelectedYear={setSelectedYear}
-            startingYear={currentYear}
-          />
-          <VacationDaysInput
-            vacationDays={vacationDays}
-            setVacationDays={setVacationDays}
-          />
-        </div>
-        <div className="button-container">
-          <Button>언제가 좋을까?</Button>
-        </div>
+        <YearPicker
+          setSelectedYear={setSelectedYear}
+          startingYear={currentYear}
+        />
+        <VacationDaysInput
+          vacationDays={vacationDays}
+          setVacationDays={setVacationDays}
+        />
+        <Button type="submit" variant="outlined">
+          언제가 좋을까?
+        </Button>
       </div>
     </form>
   );
@@ -46,46 +43,23 @@ const StyledHolidayForm = styled(HolidayForm)`
   color: ${(props) => props.theme.color.majorBrown};
 
   .center-form {
-    margin: 0 auto;
     display: flex;
-    flex-flow: column;
+    flex-direction: column;
     justify-content: space-between;
     align-items: center;
+    padding: 1rem 0;
+    margin: 0 auto;
+    > * {
+      margin: 0.75rem 0rem;
+      width: 100%;
+      max-width: 15rem;
+    }
 
     ${(props) => props.theme.media.tablet} {
-      flex-flow: row;
-      width: 70%;
-    }
-
-    ${(props) => props.theme.media.desktop} {
-      width: 60%;
-    }
-  }
-
-  .inputs-container {
-    width: 80%;
-    display: inline-flex;
-    flex: 1 1;
-    justify-content: space-between;
-    margin-bottom: 2rem;
-
-    ${(props) => props.theme.media.tablet} {
-      margin-bottom: 0;
-      max-width: 40%;
-      padding: 0;
-      flex-flow: row;
-    }
-  }
-
-  .button-container {
-    width: 70%;
-    padding: 2rem 0;
-    display: flex;
-    flex: 1 1;
-    justify-content: flex-end;
-
-    ${(props) => props.theme.media.desktop} {
-      justify-content: center;
+      flex-direction: row;
+      > * {
+        margin: 0rem 0.75rem;
+      }
     }
   }
 `;
